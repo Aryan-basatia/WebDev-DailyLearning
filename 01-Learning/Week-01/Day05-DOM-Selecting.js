e16a600b1845858f7255cc0101b1a523707b6746
Aryan-basatia <aryanbasatia24@gmail.com>
2026-02-10 06:40:08 +0530

    Add Day05 DOM selecting example
    Add HTML, CSS, and JS for the Day05 DOM Selecting lesson. Creates a demo page (Silkie Chickens) with a contents nav and several images, styles layout and image sizing in Day05-DOM-Selecting.css, and includes Day05-DOM-Selecting.js which demonstrates DOM selection methods (getElementsByTagName, getElementsByClassName, querySelector, querySelectorAll) and logs results to the console.

---
+++ 01-Learning/Week-01/Day05-DOM-Selecting.js
@@ -0,0 +1,12 @@
+allImages = document.getElementsByTagName('img');
+
+for( image of allImages){
+    console.log(image.src)
+}
+
+const squareImages = document.getElementsByClassName('square');
+console.log(squareImages);
+
+console.log(document.querySelector('a'))
+console.log(document.querySelector('p a'))
+console.log(document.querySelectorAll('p'))
