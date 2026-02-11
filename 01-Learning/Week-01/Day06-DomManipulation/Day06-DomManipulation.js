b29034c3579bbb3673638f803abc8911d3f4ee99
Aryan-basatia <aryanbasatia24@gmail.com>
2026-02-11 06:49:15 +0530

    Add Day06 DOM Manipulation example
    Add Day06 DOM Manipulation exercise: new HTML, CSS and JS demonstrating DOM structure and basic commented DOM API examples. Also add JetBrains .idea project files and a .gitignore for IDE artifacts.

---
+++ 01-Learning/Week-01/Day06-DomManipulation/Day06-DomManipulation.js
@@ -0,0 +1,13 @@
+// console.log(document.querySelector('h1').innerText = "Aryan");
+// console.log(document.querySelector('p').textContent = "Ram Ram ji");
+// console.log(document.querySelector('p').innerText)
+// console.log(document.querySelector('p').innerHTML)
+// console.log(document.querySelector('h1').innerHTML += "<sup>Aryan</sup>")
+
+
+//Attributes
+// console.log(document.querySelector('a').getAttribute('href'));
+// const input =document.querySelector('input[type ="text"]');
+
+
+
