77cc7876a48063d9372b6c6cedcada306995d5dd
Aryan-basatia <aryanbasatia24@gmail.com>
2026-02-21 20:27:35 +0530

    new

---
+++ 01-Learning/Week-03/Day15-ConstructorFunction.js
@@ -0,0 +1,49 @@
+/*
+function makeColor(r,g,b){
+    const color = {};
+    color.r = r;
+    color.g = g;
+    color.b = b;
+
+    color.rgb = function (){
+        const {r,g,b} = this;
+        return `rgb(${r},${g},${b})`;
+    };
+    color.hex = function (){
+        const {r,g,b} = this;
+        return '#'+((1 << 24) + (r << 16) + (g << 8) +b).toString(16).slice(1);
+    };
+    return color;
+}
+
+const firstColor = makeColor(25,124,22);
+firstColor.hex();
+firstColor.rgb();
+
+const black = makeColor(0,0,0);
+black.hex();
+black.rgb();*/
+
+function Color(r,g,b){
+    this.r = r;
+    this.g = g;
+    this.b = b;
+}
+Color.prototype.rgb = function (){
+    const {r,g,b} = this;
+    return `rgb(${r},${g},${b})`;
+};
+Color.prototype.hex = function (){
+    const {r,g,b} = this;
+    return '#'+((1 << 24) + (r << 16) + (g << 8) +b).toString(16).slice(1);
+};
+Color.prototype.rgba = function (a = 1.0){
+    const {r,g,b} = this;
+    return `rgba(${r},${g},${b},${a})`;
+};
+
+const colorObj1 = new Color(12,125,200);
+const colorObj2 = new Color(12,125,200);
+
+console.log(colorObj1);
+console.log(colorObj2);
